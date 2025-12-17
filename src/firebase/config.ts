c394eb43615a4c8e9bbfef6733efe6c9ecd66bc4
Premson Madduri <madduripremson@gmail.com>
2025-12-17 04:43:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3055449916-97578",
+  "appId": "1:931224557936:web:948c0817d47b474cd317b5",
+  "apiKey": "AIzaSyC0GNapq-TrgTv8aOmKMoVY7pDJMZ_twnw",
+  "authDomain": "studio-3055449916-97578.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "931224557936"
+};
