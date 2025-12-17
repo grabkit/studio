deaf6a649f6e8989487312a650e9bd571ab74d02
Premson Madduri <madduripremson@gmail.com>
2025-12-17 04:19:34 +0000

    File changes

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,5 +1,5 @@
 import { getApps, initializeApp } from 'firebase/app';
-import { getAuth, getFirestore } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
 // IMPORTANT: Replace the placeholder values with your own Firebase project configuration.
 // You can find this in your Firebase project settings under "Project settings" > "General".
@@ -9,8 +9,7 @@ const firebaseConfig = {
   "appId": "1:931224557936:web:948c0817d47b474cd317b5",
   "apiKey": "AIzaSyC0GNapq-TrgTv8aOmKMoVY7pDJMZ_twnw",
   "authDomain": "studio-3055449916-97578.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "931224557936"
+  "messagingSenderId": "931224557936",
 };
 
 // Initialize Firebase
