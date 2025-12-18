5838d54ccdb9d0379453a3d37c838202c51b933c
Premson Madduri <madduripremson@gmail.com>
2025-12-18 10:15:21 +0000

    ikada kinda description lanti text add cheyyi"Anonymous social network"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function SplashScreen() {
   }, [router]);
 
   return (
-    <div className="relative flex h-screen w-full items-center justify-center bg-background radial-spotlight">
+    <div className="relative flex h-screen w-full flex-col items-center justify-center bg-background radial-spotlight">
       <Image
         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh5r85BhL7rCkS72xpX_5xkFZ9y_fVMFXYp_zLN9eEAnEA_C61c1jCJFaG86d1W6_mtsla64B191MOWYEFhJAa-lyMikD80WyfBVKiQxyc71spJx3Oy2FgvfotsVVnNIXGRXunpHYYvGFoQ7V-URilBXwJzIV9zQLSO_PN9raerNaTAb0VuCYo9EBqiyVts/s320/New%20Project%2020%20%5BEFC25EE%5D.png"
         alt="Blur Logo"
@@ -25,6 +25,7 @@ export default function SplashScreen() {
         className="animate-pulse z-10"
         priority
       />
+      <p className="text-sm text-muted-foreground mt-4 animate-pulse font-headline">Anonymous social network</p>
     </div>
   );
 }
