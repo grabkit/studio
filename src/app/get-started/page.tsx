ba927fb6c5c3edaafc48a79cb17e1b94fb303b7c
Premson Madduri <madduripremson@gmail.com>
2025-12-18 06:49:51 +0000

    ikkada bottom lo unna image konchem paiki move cheyi

--- src/app/get-started/page.tsx
+++ src/app/get-started/page.tsx
@@ -44,7 +44,7 @@ export default function GetStartedPage() {
               </div>
             </div>
              <div className="absolute inset-0">
-                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2">
+                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/3">
                    <Image
                     src="https://picsum.photos/seed/2/50/50"
                     alt="Profile 2"
