8cda9c4577074a07eaac72764b7392637555b6bd
Premson Madduri <madduripremson@gmail.com>
2025-12-18 05:25:36 +0000

    inka komchem right ki move cheyyi

--- src/app/get-started/page.tsx
+++ src/app/get-started/page.tsx
@@ -32,7 +32,7 @@ export default function GetStartedPage() {
               />
             </svg>
             <div className="absolute inset-0">
-              <div className="absolute top-1/2 left-[-10px] -translate-y-1/2 -translate-x-1/4">
+              <div className="absolute top-1/2 left-0 -translate-y-1/2 -translate-x-1/4">
                   <Image
                     src="https://picsum.photos/seed/1/50/50"
                     alt="Profile 1"
