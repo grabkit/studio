e5b529a40d3ca6a3124b53f5320ec988ea73b827
Premson Madduri <madduripremson@gmail.com>
2025-12-18 09:46:08 +0000

    old image remove chesey,new image add cheyi "https://blogger.googleuserc

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -5,7 +5,7 @@ export default function TopBar() {
     <header className="fixed top-0 left-0 right-0 z-40 h-12 bg-background-blur border-b">
       <div className="flex items-center justify-center h-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <Image 
-          src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg-rfInAW3hffAeQ5Eid-iGK0SVwv4acB9uD6bPYkmA8jY4aKMu283s8xhbqCMUP-NIIidpel_wZbi3aarpV2wk1nH-KW3eAhSRy0f6E3Kqd-4JV0W5arP1U9TR73vEoBuit8FrMHEWhOZfSpEjw6N0gGRxknNVtIVcyYS0VUx2w4Qu3VXMYZzUyf0OQJU5/s320/New%20Project%2019%20%5BE486179%5D.png" 
+          src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh5r85BhL7rCkS72xpX_5xkFZ9y_fVMFXYp_zLN9eEAnEA_C61c1jCJFaG86d1W6_mtsla64B191MOWYEFhJAa-lyMikD80WyfBVKiQxyc71spJx3Oy2FgvfotsVVnNIXGRXunpHYYvGFoQ7V-URilBXwJzIV9zQLSO_PN9raerNaTAb0VuCYo9EBqiyVts/s320/New%20Project%2020%20%5BEFC25EE%5D.png" 
           alt="Blur Logo"
           width={100}
           height={32}
