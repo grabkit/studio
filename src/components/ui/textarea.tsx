836a31e3f75c101538c0a3bde98a2508af2c7cf3
Premson Madduri <madduripremson@gmail.com>
2025-12-19 09:45:40 +0000

    bro still issue clear avvaledhu baga analysis cheyyi

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -3,21 +3,23 @@ import TextareaAutosize from 'react-textarea-autosize';
 
 import {cn} from '@/lib/utils';
 
-const Textarea = React.forwardRef<
-  HTMLTextAreaElement,
-  React.ComponentProps<typeof TextareaAutosize>
->(({className, ...props}, ref) => {
-  return (
-    <TextareaAutosize
-      className={cn(
-        'flex w-full rounded-md border border-input bg-transparent px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm resize-none',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({className, ...props}, ref) => {
+    return (
+      <TextareaAutosize
+        className={cn(
+          'flex w-full rounded-md border border-input bg-transparent px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm resize-none',
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 Textarea.displayName = 'Textarea';
 
 export {Textarea};
