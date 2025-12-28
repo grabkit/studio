6711aa3f485dc8b4e3d34757485a1c57d8250e01
Premson Madduri <madduripremson@gmail.com>
2025-12-28 08:55:52 +0000

    Try fixing this error: `Runtime FirebaseError: Missing or insufficient p

--- src/components/ForwardSheet.tsx
+++ src/components/ForwardSheet.tsx
@@ -1,4 +1,5 @@
 
+
 "use client";
 
 import React, { useMemo, useState } from 'react';
@@ -163,3 +164,4 @@ export function ForwardSheet({ message, isOpen, onOpenChange }: { message: WithI
         </Sheet>
     )
 }
+
