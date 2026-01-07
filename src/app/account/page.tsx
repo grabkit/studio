03ab32387d8a1d58d57897c3485dd8344a5a3de4
Premson Madduri <madduripremson@gmail.com>
2026-01-07 10:23:20 +0000

    hey ikkada stil top lo unna plus icon and menu icon cliick chesteh em re

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -345,17 +345,13 @@ export default function AccountPage() {
 
         <div style={{ paddingTop: `${pullPosition}px` }} className="transition-all duration-300">
             <div className="flex items-center justify-between h-14 px-4">
-                <Button variant="ghost" size="icon" asChild>
-                    <Link href="/post">
-                        <Plus className="h-6 w-6" />
-                    </Link>
-                </Button>
+                 <Link href="/post" className={cn(buttonVariants({ variant: "ghost", size: "icon" }))}>
+                    <Plus className="h-6 w-6" />
+                </Link>
                 <div className="flex items-center space-x-2">
-                    <Button asChild variant="ghost" size="icon">
-                      <Link href="/account/settings">
-                          <Menu className="h-6 w-6" />
-                      </Link>
-                    </Button>
+                    <Link href="/account/settings" className={cn(buttonVariants({ variant: "ghost", size: "icon" }))}>
+                      <Menu className="h-6 w-6" />
+                    </Link>
                 </div>
             </div>
             <div className="px-4">
@@ -491,5 +487,7 @@ export default function AccountPage() {
 
 
 
+
+    
 
     
