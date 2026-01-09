bbf117adb5311ae3df3c6b9876134422bdeff440
Premson Madduri <madduripremson@gmail.com>
2026-01-09 03:10:08 +0000

    bro ikkada border radius normal gaa bars ki left and right lo undaali,bu

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -142,7 +142,7 @@ export function PollComponent({ post, user }: { post: WithId<Post>, user: any })
 
                 if (hasVoted) {
                      return (
-                        <div key={index} className="relative w-full h-10 overflow-hidden bg-secondary">
+                        <div key={index} className="relative w-full h-10 overflow-hidden bg-secondary rounded-full">
                              <div
                                 className={cn(
                                     "absolute left-0 top-0 h-full transition-all duration-500 ease-out",
