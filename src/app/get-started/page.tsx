90d8b28c08f1ee1778ad08b4e7e20c85fafbed74
Premson Madduri <madduripremson@gmail.com>
2026-01-11 09:00:01 +0000

    ikkada oko screen size lo perfect gaane show avuthunnayi,screen size chn

--- src/app/get-started/page.tsx
+++ src/app/get-started/page.tsx
@@ -69,53 +69,49 @@ export default function GetStartedPage() {
                 strokeDasharray="4"
               />
             </svg>
-            {/* Outer Circle Images */}
-            <div className="absolute inset-0 animate-rotate-around">
-              <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 h-12 w-12 animate-rotate-around-reverse">
+            
+            {/* Avatars */}
+            <div className="absolute top-1/2 left-1/2 h-12 w-12 animate-rotate-outer-1">
                 <div className="relative h-full w-full rounded-full overflow-hidden blur-[2px]">
                     <Image
-                      src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjl__yIIdDpdUqwihL8vBMfr5ioet6tuh425rGFdshYyY6Fpa6_gy7tOHWaLzkfAi45dzjPXJu6uRteAIc-Z6TQav-LRFG8-SDCAfL2wQGBM1URWSM1gglLO8MwvYkyMW4JinOcQ3WOHZ2QB3GGhIxrXwKQOomSukzxewoONh2iQAiLlyhJNwUg_i714ihv/s320/demo%20profile%205.png"
-                      alt="Profile 1"
-                      fill
-                      className="object-cover"
+                        src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjl__yIIdDpdUqwihL8vBMfr5ioet6tuh425rGFdshYyY6Fpa6_gy7tOHWaLzkfAi45dzjPXJu6uRteAIc-Z6TQav-LRFG8-SDCAfL2wQGBM1URWSM1gglLO8MwvYkyMW4JinOcQ3WOHZ2QB3GGhIxrXwKQOomSukzxewoONh2iQAiLlyhJNwUg_i714ihv/s320/demo%20profile%205.png"
+                        alt="Profile 1"
+                        fill
+                        className="object-cover"
                     />
                 </div>
-              </div>
-                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 h-12 w-12 animate-rotate-around-reverse">
-                   <div className="relative h-full w-full rounded-full overflow-hidden blur-[2px]">
-                      <Image
+            </div>
+             <div className="absolute top-1/2 left-1/2 h-12 w-12 animate-rotate-outer-2">
+                <div className="relative h-full w-full rounded-full overflow-hidden blur-[2px]">
+                    <Image
                         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiRFKVT6Hq5aKq8lP0EfeFTQMvGBMBD7Qcn5a347eq19vSsX3OLXIZ80Ggh9Ucw2OkMAoCNjs7kGzd5QdLbtJ5kczINsGyHt11ZRJTc7lzlPi1-etHHdZuW0xFoYMdEPk7IIqgM15h7cY-sngmh6r-59c5Itnqfx3GNthAMeIwiFMYFKNtQOO293QOnixie/s320/demo%20profile%204.jpg"
                         alt="Profile 2"
                         fill
                         className="object-cover"
-                      />
-                  </div>
+                    />
                 </div>
             </div>
-
-            {/* Inner Circle Images */}
-            <div className="absolute inset-0 animate-rotate-around-reverse">
-                <div className="absolute top-1/2 left-0 -translate-y-1/2 -translate-x-1/2 h-10 w-10 animate-rotate-around">
-                   <div className="relative h-full w-full rounded-full overflow-hidden blur-[2px]">
-                      <Image
+            <div className="absolute top-1/2 left-1/2 h-10 w-10 animate-rotate-inner-1">
+                <div className="relative h-full w-full rounded-full overflow-hidden blur-[2px]">
+                    <Image
                         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjBmZb3uykpKpaW3VGWkJdOMssZrbME9MvIYmfluO0aTDa9QAOdimTBQOm8fwQDawffB2sUJKPN2W5xaSzVWTtVBFCWEbbPkuHz9cFBZum8tx8aEyHakc0GLme-QvLwyJyJnokU1Ozb2dLwZuZPddj2w25s38yepCYzfGuaK14cFcA1JLPHz1tTJQxs5Tv8/s320/demo%20profile%203.jpg"
                         alt="Profile 3"
                         fill
                         className="object-cover"
-                      />
-                  </div>
+                    />
                 </div>
-                <div className="absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/2 h-10 w-10 animate-rotate-around">
-                   <div className="relative h-full w-full rounded-full overflow-hidden blur-[2px]">
-                      <Image
+            </div>
+             <div className="absolute top-1/2 left-1/2 h-10 w-10 animate-rotate-inner-2">
+                <div className="relative h-full w-full rounded-full overflow-hidden blur-[2px]">
+                    <Image
                         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjuiyp3nmqkOPU38Mrqn6WhSS9ZVqUBOHCq439-DNdXxR8JdGWTDcq97V-6P1KHTO9W4F7Udoh9ja5rBw1gPsINICTlUqaadNfajnTmvxCPOfju-0cbw-dMAkvTtvMBWjC9TiI7pnFXaO2YyxP2fiRxlx9wYL20VrhBmH5tXhac7LxJsYmirLN3z7JyMW9I/s320/demo%20profile%202.webp"
                         alt="Profile 4"
                         fill
                         className="object-cover"
-                      />
-                  </div>
+                    />
                 </div>
             </div>
+
 
              <Image
                 src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh5r85BhL7rCkS72xpX_5xkFZ9y_fVMFXYp_zLN9eEAnEA_C61c1jCJFaG86d1W6_mtsla64B191MOWYEFhJAa-lyMikD80WyfBVKiQxyc71spJx3Oy2FgvfotsVVnNIXGRXunpHYYvGFoQ7V-URilBXwJzIV9zQLSO_PN9raerNaTAb0VuCYo9EBqiyVts/s320/New%20Project%2020%20%5BEFC25EE%5D.png"
