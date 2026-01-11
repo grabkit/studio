8f476bef28c931f6ace39ad8755d899f9effb1c1
Premson Madduri <madduripremson@gmail.com>
2026-01-11 04:25:27 +0000

    now ikkada "get-started" button konchem kindaki move cheyyi

--- src/app/get-started/page.tsx
+++ src/app/get-started/page.tsx
@@ -151,7 +151,7 @@ export default function GetStartedPage() {
               ))}
           </div>
         </div>
-        <Button asChild size="lg" className="w-full font-headline text-lg rounded-full mt-24">
+        <Button asChild size="lg" className="w-full font-headline text-lg rounded-full mt-32">
           <Link href="/auth">
             Get Started
             <ArrowRight className="ml-2 h-5 w-5" />
