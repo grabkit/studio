fa2dcdc97b5c93fd99e8c6090b8d683eb3f9634e
Premson Madduri <madduripremson@gmail.com>
2026-01-11 09:49:49 +0000

    ippudu animation stop chesaka images two lines lo correct position lo un

--- src/app/get-started/page.tsx
+++ src/app/get-started/page.tsx
@@ -49,7 +49,7 @@ export default function GetStartedPage() {
     >
        <div className="background-grid"></div>
       <div className="relative w-[320px] h-[320px] flex items-center justify-center mb-16">
-            <svg width="320" height="320" viewBox="0 0 320 320" className="absolute">
+            <svg width="320" height="320" viewBox="0 0 320 320" className="absolute animate-spin-slow">
               <circle
                 cx="160"
                 cy="160"
@@ -71,7 +71,7 @@ export default function GetStartedPage() {
             </svg>
             
             {/* Avatars */}
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" style={{ transform: 'translate(-50%, -50%) translateY(-150px)' }}>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-rotate-outer-1">
                 <div className="relative h-12 w-12 rounded-full overflow-hidden blur-[2px]">
                     <Image
                         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjl__yIIdDpdUqwihL8vBMfr5ioet6tuh425rGFdshYyY6Fpa6_gy7tOHWaLzkfAi45dzjPXJu6uRteAIc-Z6TQav-LRFG8-SDCAfL2wQGBM1URWSM1gglLO8MwvYkyMW4JinOcQ3WOHZ2QB3GGhIxrXwKQOomSukzxewoONh2iQAiLlyhJNwUg_i714ihv/s320/demo%20profile%205.png"
@@ -81,7 +81,7 @@ export default function GetStartedPage() {
                     />
                 </div>
             </div>
-             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" style={{ transform: 'translate(-50%, -50%) translateY(150px)' }}>
+             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-rotate-outer-2">
                 <div className="relative h-12 w-12 rounded-full overflow-hidden blur-[2px]">
                     <Image
                         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiRFKVT6Hq5aKq8lP0EfeFTQMvGBMBD7Qcn5a347eq19vSsX3OLXIZ80Ggh9Ucw2OkMAoCNjs7kGzd5QdLbtJ5kczINsGyHt11ZRJTc7lzlPi1-etHHdZuW0xFoYMdEPk7IIqgM15h7cY-sngmh6r-59c5Itnqfx3GNthAMeIwiFMYFKNtQOO293QOnixie/s320/demo%20profile%204.jpg"
@@ -91,7 +91,7 @@ export default function GetStartedPage() {
                     />
                 </div>
             </div>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" style={{ transform: 'translate(-50%, -50%) translateX(-100px)' }}>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-rotate-inner-1">
                 <div className="relative h-10 w-10 rounded-full overflow-hidden blur-[2px]">
                     <Image
                         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjBmZb3uykpKpaW3VGWkJdOMssZrbME9MvIYmfluO0aTDa9QAOdimTBQOm8fwQDawffB2sUJKPN2W5xaSzVWTtVBFCWEbbPkuHz9cFBZum8tx8aEyHakc0GLme-QvLwyJyJnokU1Ozb2dLwZuZPddj2w25s38yepCYzfGuaK14cFcA1JLPHz1tTJQxs5Tv8/s320/demo%20profile%203.jpg"
@@ -101,7 +101,7 @@ export default function GetStartedPage() {
                     />
                 </div>
             </div>
-             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" style={{ transform: 'translate(-50%, -50%) translateX(100px)' }}>
+             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-rotate-inner-2">
                 <div className="relative h-10 w-10 rounded-full overflow-hidden blur-[2px]">
                     <Image
                         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjuiyp3nmqkOPU38Mrqn6WhSS9ZVqUBOHCq439-DNdXxR8JdGWTDcq97V-6P1KHTO9W4F7Udoh9ja5rBw1gPsINICTlUqaadNfajnTmvxCPOfju-0cbw-dMAkvTtvMBWjC9TiI7pnFXaO2YyxP2fiRxlx9wYL20VrhBmH5tXhac7LxJsYmirLN3z7JyMW9I/s320/demo%20profile%202.webp"
