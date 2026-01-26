33f4764689cd70dee89ed2c45187b6f2b8d0fcfa
Premson Madduri <madduripremson@gmail.com>
2026-01-26 04:39:39 +0000

    bro inkA  konchem kindaki move cheyyi

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -23,7 +22,7 @@ const Avatar = React.forwardRef<
       {...props}
     />
      {showStatus && isOnline && (
-      <span className="absolute top-0.5 right-0 block h-3 w-3 rounded-full bg-[hsl(var(--online-glow-color))] animate-online-indicator-glow border-2 border-background" />
+      <span className="absolute top-1 right-0 block h-3 w-3 rounded-full bg-[hsl(var(--online-glow-color))] animate-online-indicator-glow border-2 border-background" />
     )}
   </div>
 ))
